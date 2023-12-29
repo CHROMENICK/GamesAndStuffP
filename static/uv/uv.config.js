7e1382d2464fac0a8b488bbbda5faa35e648687f
gamachikin <102493636+gamachikin@users.noreply.github.com>
2023-12-29 13:42:34 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,12 +1,10 @@
-var bareServers = ["bare1"];
-var bareDomain = "https://" + bareServers[Math.floor(Math.random() * bareServers.length)] + "." + self.location.hostname + "/bare/";
-
 self.__uv$config = {
     prefix: '/reviews/',
-    bare: bareDomain,
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
+    client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
     sw: '/uv/sw.js',
